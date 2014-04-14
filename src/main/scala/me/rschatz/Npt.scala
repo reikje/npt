460a2c69aefca206a4b7273a5dff098c35c861ab
rschatz <rschatz@ea.com>
2014-04-14 15:57:37 +0200

    implementing plugin

--- src/main/scala/me/rschatz/Npt.scala
+++ src/main/scala/me/rschatz/Npt.scala
@@ -6,6 +6,8 @@ import complete.DefaultParsers._
 import java.io.File
 import scala.util.matching.Regex
 import java.lang.Boolean._
+import java.net.URL
+import scala.util.{Success, Failure, Try}
 
 /**
  * A SBT plugin to initialize new projects based on existing project templates.
@@ -17,10 +19,31 @@ object Defaults {
     val name = "default-name"
 }
 
-case class NptExecutionSettings(baseDirectory: File)
+case class NptExecutionContext(baseDirectory: File, args: Seq[String], log: sbt.Logger) {
+    def inputArgs() = {
+        val Org   = """org\:(\S+)""".r
+        val Name  = """name\:(\S+)""".r
+
+        val orgValue = args.collect({
+            case Org(inputOrg) => Some(inputOrg) }
+        ).headOption.getOrElse(Some(Defaults.organization))
+
+        val nameValue = args.collect({
+            case Name(inputName) => Some(inputName) }
+        ).headOption.getOrElse(Some(Defaults.name))
+
+        val templateValue = args.filter(!Seq(orgValue, nameValue).contains(_)).map(s => Some(s)).headOption.getOrElse(None)
+
+        (orgValue, nameValue, templateValue)
+    }
+}
 
 object Npt extends Plugin {
     private val npt = InputKey[Unit]("npt")
+
+    private val nptCopy = InputKey[Unit]("npy-copy")
+
+    private val DownloadableArchive = "^(https?|ftp|file)://.+\\.(gz|zip|jar)$".r
 
     private val sourceDirName = "src"
     private val mainDirName = "main"
@@ -30,43 +53,182 @@ object Npt extends Plugin {
     private val resourcesDirName = "resources"
     private val buildFileName = "build.sbt"
 
+    private val defaultTemplate = "SBT.NPT.DEFAULT.TEMPLATE"
+    private val templateFolder = "SBT.NPT.TEMPLATE.FOLDER"
+
     val nptSettings = Seq(
         npt := {
             val nptArgs: Seq[String] = spaceDelimited("<arg>").parsed
             println(nptArgs)
 
-            implicit val execSettings = NptExecutionSettings(baseDirectory.value)
+            implicit val execContext = NptExecutionContext(baseDirectory.value, nptArgs, streams.value.log)
             createSrcDirectories()
-            createBuildSbt(nptArgs)
+            createBuildSbt()
+            copyTemplate()
+        },
+        nptCopy := {
+            val nptArgs: Seq[String] = spaceDelimited("<arg>").parsed
+            implicit val execContext = NptExecutionContext(baseDirectory.value, nptArgs, streams.value.log)
+            copyTemplate()
         }
     )
 
-    private def createSrcDirectories()(implicit es: NptExecutionSettings) = {
+    private def copyTemplate()(implicit es: NptExecutionContext): Unit = {
+        val log = es.log
+        log.info(s"Finding template to copy")
+
+        var folderToCopy: Option[File] = None
+        val defaultTemplate = fromDefaultTemplate()
+        if (defaultTemplate.isDefined) {
+            folderToCopy = defaultTemplate
+        } else {
+            val defaultFolder = fromDefaultFolder()
+            if (defaultFolder.isDefined) {
+                folderToCopy = defaultFolder
+            } else {
+                val fromInput = fromInputArgs()
+                if (fromInput.isDefined) {
+                    folderToCopy = fromInput
+                }
+            }
+        }
+        
+        if (folderToCopy.isDefined) {
+            IO.copyDirectory(folderToCopy, es.baseDirectory)
+        } else {
+            log.info(s"Not copying a template")
+        }
+    }
+    
+    private def fromInputArgs()(implicit es: NptExecutionContext): Option[File] = {
+        val log = es.log
+
+        val (_, _, templateNameOption) = es.inputArgs()
+        if (templateNameOption.isDefined) {
+            val templateName = templateNameOption.get
+            log.info(s"Trying template name or location from input ($templateName)")
+
+            templateName match {
+                case DownloadableArchive(protocol, extension) => downloadTemplate(templateName)
+                case _ => templateFolder(new File(templateName))
+            }
+        } else {
+            None
+        }
+    }
+    
+
+    private def fromDefaultTemplate()(implicit es: NptExecutionContext): Option[File] = {
+        // check SBT.NPT.DEFAULT.TEMPLATE
+        val log = es.log
+
+        for (props <- List(sys.env, sys.props)) {
+            val defaultTemplateName = props.get(defaultTemplate)
+            if (defaultTemplateName.isDefined) {
+                val defaultTemplateNameValue = defaultTemplateName.get
+                log.info(s"Trying $defaultTemplate ($defaultTemplateNameValue)")
+                return downloadTemplate(defaultTemplateNameValue)
+            }
+        }
+        None
+    }
+
+    private def fromDefaultFolder()(implicit es: NptExecutionContext): Option[File] = {
+        // check SBT.NPT.TEMPLATE.FOLDER
+        // if SBT.NPT.TEMPLATE.FOLDER => get template name from args and copy this template
+        val log = es.log
+
+        for (props <- List(sys.env, sys.props)) {
+            val templateFolderName = props.get(templateFolder)
+            if (templateFolderName.isDefined) {
+                val templateFolderNameValue = templateFolderName.get
+                log.info(s"Trying $templateFolder ($templateFolderNameValue)")
+                val folder = new File(templateFolderNameValue)
+                if (folder.exists()) {
+                    val (_, _, templateNameOption) = es.inputArgs()
+                    if (templateNameOption.isDefined) {
+                        val templateName = templateNameOption.get
+                        return templateFolder(new File(folder, templateName))
+                    } else {
+                        log.info(s"Missing name of a template in folder $templateFolderNameValue")
+                    }
+                } else {
+                    log.info(s"Folder: $templateFolderName is not an existing folder")
+                }
+            }
+        }
+        None
+    }
+
+    private def templateFolder(directory: File)(implicit es: NptExecutionContext): Option[File] = {
+        val log = es.log
+        if (directory.isDirectory) {
+            log.info(s"Existing folder $directory")
+            Some(directory)
+        } else {
+            log.info(s"$directory does not exist or is not a directory. Skipping")
+            None
+        }
+    }
+
+    private def downloadTemplate(url: String)(implicit es: NptExecutionContext): Option[File] = {
+        val log = es.log
+
+        def download(url: String): Try[File] = {
+            url match {
+                case DownloadableArchive(protocol, extension) =>
+                    val workFolder = "NPT-DOWNLOAD"
+                    val targetFolder = new File(IO.temporaryDirectory, workFolder)
+                    if (targetFolder.exists()) {
+                        log.info(s"Deleting pre-existing temporary folder")
+                        IO.delete(targetFolder)
+                    }
+
+                    log.info(s"Creating temporary folder ($targetFolder)")
+                    IO.createDirectory(targetFolder)
+
+                    log.info(s"Unzipping to temporary folder")
+
+                    try {
+                        val templateURL = new URL(url)
+                        val files = IO.unzipURL(templateURL, targetFolder).toList
+                        files match {
+                            case Nil => Failure(new IllegalStateException(s"Archive could not be downloaded or contains no files"))
+                            case _ => Success(targetFolder)
+                        }
+                    } catch {
+                        case e: Throwable => Failure(e)
+                    }
+                case _ => Failure(new IllegalArgumentException(s"URL: $url does not denote a downloadable archive"))
+            }
+        }
+
+        download(url) match {
+            case Success(f) => Some(f)
+            case Failure(e) =>
+                log.info(e.getMessage)
+                None
+        }
+    }
+
+    private def createSrcDirectories()(implicit es: NptExecutionContext) = {
+        val log = es.log
+        log.info("Creating source folders")
+
         IO.createDirectories(sourceDirs(es.baseDirectory))
     }
 
-    private def createBuildSbt(nptArgs: Seq[String])(implicit es: NptExecutionSettings) = {
-        val org   = """org\:(\S+)""".r
-        val name  = """name\:(\S+)""".r
+    private def createBuildSbt()(implicit es: NptExecutionContext) = {
+        val log = es.log
+        log.info("Creating build.sbt")
 
-        def matchOrDefault[T](default: T)(pf: PartialFunction[String, T]) =
-            nptArgs.collect(pf).headOption.getOrElse(default)
-
-        val (orgValue, nameValue) = (
-            matchOrDefault(Defaults.organization) {
-                case org(inputOrg)   => inputOrg
-            },
-            matchOrDefault(Defaults.name) {
-                case name(inputName)  => inputName
-            }
-        )
-
+        val (orgValueOption, nameValueOption, _) = es.inputArgs()
         IO.writeLines(
             new File(es.baseDirectory, buildFileName),
             List(
-                "organization := \"%s\"".format(orgValue),
+                "organization := \"%s\"".format(orgValueOption.get),
                 "",
-                "name := \"%s\"".format(nameValue),
+                "name := \"%s\"".format(nameValueOption.get),
                 "",
                 "version := \"0.1-SNAPSHOT\"",
                 "",
